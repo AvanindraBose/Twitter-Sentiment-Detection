8f63ff324550a04b26b34158b9fc72ef1ee48e41
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-02 00:54:07 +0530

    Initialized the Structure of the Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is an E2E MLOps solution which will detect the emotions after reading comments',
+    author='Avanindra Bose',
+    license='MIT',
+)
