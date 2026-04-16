9f5f75afb0e53c73f7f6d9cdc231dd2b3e076ce5
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-17 02:37:16 +0530

    Successfully Integrated Basic Frontend Backend Infrastructure using FASTAPI and HTML

--- backend/api/routes_predict.py
+++ backend/api/routes_predict.py
@@ -1,25 +1,32 @@
-from fastapi import APIRouter,HTTPException,status
+import os
+from fastapi import APIRouter,HTTPException,status,Request,Form
 from backend.schema.model_schema import RequestSchema,ResponseSchema
 from backend.services.model_service import predict_sentiment
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 router = APIRouter(tags=["Predict"])
+templates = Jinja2Templates(
+    directory= "backend/templates"
+)
 
-@router.get("/")
-def root():
-    return {"message": "Welcome to the Twitter Sentiment Detection API!"}
+@router.get("/",response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse(request = request , name = "index.html")
 
-@router.post("/predict")
-def prediction(request:RequestSchema) -> ResponseSchema:
+@router.post("/predict",response_class=HTMLResponse)
+def prediction(request:Request , text:str = Form(...)):
     # 3 Stages are required before Prediction
     # Validate the input data -> Handled by Pydantic
     # Preprocess the input data using vectorizer
     # Make predictions using the loaded model
     try:
-        data = {"text" : request.text}
-        prediction = predict_sentiment(data)
-        return {"sentiment": prediction['prediction']}
+        data = {"text" : text}
+        result = predict_sentiment(data)
+        return templates.TemplateResponse(
+        request=request,
+        name="result.html",
+        context={"sentiment": result['prediction']}
+    )
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-
