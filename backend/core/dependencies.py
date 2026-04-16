da03bba116ec191181e18d084b1066af4ef5a4ba
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-17 01:03:38 +0530

    IMplemented Lazy Loading of the model with optimized codes

--- backend/core/dependencies.py
+++ backend/core/dependencies.py
@@ -8,16 +8,22 @@ mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
 dagshub.init(repo_owner='AvanindraBose', repo_name='Twitter-Sentiment-Detection', mlflow=True)
 client = MlflowClient()
 
+_model,_vectorizer = None,None
+
 def load_artifacts() -> tuple:
+    global _model, _vectorizer
+    if _model is not None and _vectorizer is not None:
+        return (_model, _vectorizer)
+
     prod_model = client.get_latest_versions("model", stages=["Production"])[0]
     run_id = prod_model.run_id
     model_uri = f"models:/model/{prod_model.version}"
 
-    model = mlflow.pyfunc.load_model(model_uri)
+    _model = mlflow.pyfunc.load_model(model_uri)
 
     vectorizer_uri = f"runs:/{run_id}/vectorizer.joblib"
     local_path = mlflow.artifacts.download_artifacts(vectorizer_uri)
 
-    vectorizer = joblib.load(local_path)
+    _vectorizer = joblib.load(local_path)
 
-    return (model, vectorizer)
+    return (_model, _vectorizer)
