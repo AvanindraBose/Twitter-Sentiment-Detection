437df9a0916b42e967ebf06af23612f02116a199
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-18 19:20:19 +0530

    Integrated the recnt developments in main.py file

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,25 @@
 import logging
 from fastapi import FastAPI
-from backend.api import routes_predict
+from backend.api import routes_predict,routes_auth,routes_health
+from backend.core.database import engine,Base
+from backend.middlewares.response_logger import ResponseLoggerMiddleware
 from fastapi.staticfiles import StaticFiles
 from src.logger_class import CustomLogger , create_log_path
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    # Shutdown
+    await engine.dispose()
 
 app = FastAPI(title="Twitter Sentiment Detection API", description="API for detecting sentiment in tweets", version="1.0")
+app.add_middleware(ResponseLoggerMiddleware)
 
 log_path = create_log_path("prediction")
 
