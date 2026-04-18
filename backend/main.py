0d3cce145e8474c4c2ba06c07f998dcec7c2f74a
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-18 06:18:12 +0530

    Frontend HTML & CSS FIles are completed.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from backend.api import routes_predict
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Twitter Sentiment Detection API", description="API for detecting sentiment in tweets", version="1.0")
-
+app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 app.include_router(routes_predict.router , tags=["Predict"])
