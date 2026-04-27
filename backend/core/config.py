3160d059794e4ad2bc8ae7a8c02cc158a281af89
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-28 02:07:33 +0530

    Changed Config File to handle the API Key secret

--- backend/core/config.py
+++ backend/core/config.py
@@ -6,14 +6,14 @@ load_dotenv()
 class Settings:
     PROJECT_NAME = "IRIS SPECIES CLASSIFIER"
 
-    API_KEY = os.environ["API_KEY"]
-    JWT_ACCESS_SECRET_KEY = os.environ["JWT_ACCESS_SECRET_KEY"]
-    JWT_REFRESH_SECRET_KEY = os.environ["JWT_REFRESH_SECRET_KEY"]
+    API_KEY = os.getenv("API_KEY")
+    JWT_ACCESS_SECRET_KEY = os.getenv("JWT_ACCESS_SECRET_KEY")
+    JWT_REFRESH_SECRET_KEY = os.getenv("JWT_REFRESH_SECRET_KEY")
 
     JWT_ALGORITHM = "HS256"
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
-    MLFLOW_TRACKING_URI = os.environ["MLFLOW_TRACKING_URI"]
+    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
     MODEL_NAME = "IrisRandomForest"
-    DATABASE_URL = os.environ["DATABASE_URL"]
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
 settings = Settings()
